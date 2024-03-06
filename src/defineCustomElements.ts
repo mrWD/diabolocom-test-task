163de18217a1343ae7325178b2892cac5ec5216e
mrWD <lvigtor@gmail.com>
2024-03-06 12:48:13 +0100

    Remove defineCustomElements.ts

--- src/defineCustomElements.ts
+++
@@ -1,9 +0,0 @@
-import { defineCustomElement } from 'vue'
-
-export const defineCustomElements = (components: Record<string, object>) => {
-  for (const componentName in components) {
-    const btn = defineCustomElement(components[componentName])
-
-    customElements.define(componentName, btn)
-  }
-}
